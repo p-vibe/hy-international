ad1b05aa8fca1b872ad849ed6e40c45603d550e6
junk-sound <wom2277@naver.com>
2020-10-17 20:27:13 +0900

    feat: eslint, prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,16 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
-  parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  extends: [
+    '@react-native-community',
+    'airbnb-typescript',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'prettier/react'
+  ],
+  rules: {
+    '@typescript-eslint/no-use-before-define': 'off',
+    '@typescript-eslint/no-loop-func': 'off',
+    '@typescript-eslint/no-redeclare': 'off',
+    '@typescript-eslint/no-shadow': 'off'
+  }
 };

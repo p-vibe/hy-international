9db2b3e3f678138706dab246e6c194a6314cf730
junk-sound <wom2277@naver.com>
2020-11-19 00:42:26 +0900

    feat: 웹소켓 연결

--- src/components/section/ChatSection.tsx
+++ src/components/section/ChatSection.tsx
@@ -1,10 +1,43 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { GiftedChat, IMessage } from 'react-native-gifted-chat';
+import { Stomp } from '@stomp/stompjs';
+import SockJS from 'sockjs-client';
+import 'text-encoding-polyfill';
 
+// todo: refac
 const ChatSection: React.FC = () => {
   const [messages, setMessages] = useState<IMessage[]>([]);
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 3,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any'
+        }
+      }
+    ]);
+  }, []);
+
+  const ws = Stomp.over(() => new SockJS('http://localhost:8080/ws-stomp'));
+  const headers = {
+    'accept-version': '1.2'
+  };
 
   function send(newMessages: IMessage[]) {
+    ws.connect(headers, () => {
+      ws.subscribe(
+        `/sub/chat/room/11084123-e6fb-4191-8fd8-5674a5107c33`,
+        () => {},
+        {
+          id: '1',
+          ack: 'client'
+        }
+      );
+    });
     setMessages(GiftedChat.append(messages, newMessages));
   }
 
